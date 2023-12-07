630782d84fdf7c57dbaa46d38ed4b07cd05d102f
Meet Parmar <ywcameet22@gmail.com>
2023-12-07 08:07:11 +0530

    Add files via upload

---
+++ meet/web_info.php
@@ -0,0 +1,9 @@
+E-Courts : Web Information Manager
+Name: Ms R. Arulmozhiselvi
+
+Designation: Member – Human Resources
+
+Department: eCommittee, Supreme Court of India
+
+Email: hr-ecommittee[at]aij[dot]gov[dot]in
+
